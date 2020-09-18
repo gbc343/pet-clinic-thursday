0a037a62daf4e76af8921c5f67562a6d27a1278d
gbc343 <gordonwells@hotmail.com>
2020-09-18 00:54:51 -0400

    First commit for pet-clinic Thursday

---
+++ src/main/java/ca/gbc/comp3095/petclinicthursday/PetClinicThursdayApplication.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.petclinicthursday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicThursdayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicThursdayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/gbc/comp3095/petclinicthursday/PetClinicThursdayApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.petclinicthursday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicThursdayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
